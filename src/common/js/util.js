4a2536781a4da67180131ad4ac653162fbc4737b
skyworth <skyworth@skyworthdeiMac.local>
2018-02-01 11:39:22 +0800

    ok

--- src/common/js/util.js
+++ src/common/js/util.js
@@ -1,11 +1,12 @@
 // 随机数生成函数
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random * (max - min + 1) + min)
 }
 
+// 数组洗牌函数
 export function shuffle(arr) {
-  let _arr = arr.slice()
-  for (let i = 0; i < _arr.length; i++) {
+  let _arr = arr.slice()//创建arr副本，不会直接修改原数组顺序
+  for (let i = 0; i < _arr.lenth; i++) {
     let j = getRandomInt(0, i)
     let t = _arr[i]
     _arr[i] = _arr[j]
