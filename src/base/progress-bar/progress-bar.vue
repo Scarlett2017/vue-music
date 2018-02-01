15837bd925580bf9c6b6a5e2b1b382e86f182523
skyworth <skyworth@skyworthdeiMac.local>
2018-02-01 10:49:01 +0800

    ok

--- src/base/progress-bar/progress-bar.vue
+++ src/base/progress-bar/progress-bar.vue
@@ -51,7 +51,12 @@
         this._triggerPercent()
       },
       progressClick(e){
-        this._offset(e.offsetX)
+        const rect = this.$refs.progressBar.getBoundingClientRect
+        //DOMRect 对象包含了一组用于描述边框的只读属性——left、top、right和bottom，单位为像素。除了 width 和 height 外的属性都是相对于视口的左上角位置而言的。
+        const offsetWidth = e.pageX - rect.left
+        this._offset(offsetWidth)
+        // 当点击progressBtn时，e.offsetX获取不对
+        // this._offset(e.offsetX)
         this._triggerPercent()
       },
       _offset(offsetWidth) {
