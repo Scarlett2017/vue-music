3b733f84fba3a17663a8189fb8fba60457fe5317
skyworth <skyworth@skyworthdeiMac.local>
2018-02-01 11:49:50 +0800

    ok

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -6,7 +6,7 @@
     <h1 class="title">{{title}}</h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="play-wrapper">
-        <div class="play" v-show="songs.length > 0" ref="playBtn" @click="randomPlay">
+        <div class="play" v-show="songs.length > 0" ref="playBtn" @click="random">
           <i class="icon-play"></i>
           <span class="text">随机播放全部</span>
         </div>
@@ -84,7 +84,7 @@ export default {
           index
         })
     },
-    randomPlay(){
+    random(){
       this.randomPlay({
         list:this.songs
       })
